c56335191a551ed45d6eb1c87c17587aa603e0b3
RyanMattera <34049433+RyanMattera@users.noreply.github.com>
2018-11-18 14:50:00 -0500

    Add files via upload
    businessClass

---
+++ businessClass.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+class businessClass {
+
+//Business Class for contacts app
+
+    //Attributes
+    private string businessID;
+    private string businessName;
+    private string Industry;
+    private string Address;
+    private string email;
+    private string phoneNumber;
+    private date firstContactDate;
+    private string contactSource;
+
+
+    businessClass(string businessID) {
+
+    }
+
+}
+
+
